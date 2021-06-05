6f7a279c16ef90be5db5d0f7d3b57dc410b652b1
saclod_github_007 <74409287+neil-github-007@users.noreply.github.com>
2021-06-05 15:51:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 var database; 
 var playerCount = 0;
 var gameState = 0;
-var form, player, choose;
-var chosenCount = 0;
+var form, player;
+
 var level1Count = 0;
 
 var introImage, level1_bg;
 var button_sound, hell_sound;
 
-var character1, character2, character3;
+
 
 function preload() {
     introImage = loadImage("images/intro.jpg");
@@ -28,37 +28,12 @@ function setup() {
     game.start();
 
     player = new Player();
-
-    character1 = createSprite(200, displayHeight - 500, 50, 50);
-    character1.shapeColor = "red";
-
-    character2 = createSprite(700, displayHeight - 500, 50, 50);
-    character2.shapeColor = "red";
-
-    character3 = createSprite(1200, displayHeight - 500, 50, 50);
-    character3.shapeColor = "red";
-
+    
+    
 }
 
 function draw() {
-    if(playerCount === 3) {
-        game.update(1);
+    if (playerCount === 3) {
+        game.update(1)
     }
-    if(gameState === 1) {
-        game.choose();
-    }
-    if(chosenCount === 3) {
-        game.update(2);
-    }
-    if(gameState === 2) {
-        game.startLevel1();
-    }
-    if(gameState !== 2) {
-        game.level1.hide();
-        game.description.hide();
-        game.controls.hide();
-        game.play.hide();
-    }
-
 }
-
